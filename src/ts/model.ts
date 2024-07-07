a7b38acf84c9cdbe7ac50831742479e256c4c094
thenaijagamer <sabdullahialaba50@gmail.com>
2024-07-07 03:38:46 +0100

    feat: Add implementation to get weather icon name

--- src/ts/model.ts
+++ src/ts/model.ts
@@ -1,6 +1,7 @@
 import { API_KEY } from "./config";
 
 export const state = {
+  weatherData: {},
   search: {
     query: "",
     results: [],
@@ -9,8 +10,11 @@ export const state = {
 
 type WeatherObject = {
   city: string;
-  temp: number; // Celsius
+  tempC: number; // Celsius
+  tempF: number; // Celsius
   weatherIcon: string;
+  weatherDescription: string;
+  weatherID: number;
   date: number; // Unix timestamp
   windStatus: number; // meters/second
   windDirection: number; // degrees
@@ -22,15 +26,58 @@ type WeatherObject = {
 const createWeatherObject = (data: any): WeatherObject => {
   return {
     city: data.name,
-    temp: data.main.temp - 273.15, // Converting Kelvin to Celsius
+    tempC: data.main.temp - 273.15, // Converting Kelvin to Celsius
+    tempF: ((data.main.temp - 273.15) * 9) / 5 + 32, // Converting Kelvin to Fahrenheit
     weatherIcon: data.weather[0].icon,
+    weatherDescription: data.weather[0].description,
+    weatherID: data.weather[0].id,
     date: data.dt,
-    windStatus: data.wind.speed, // meters/second
+    windStatus: data.wind.speed * 2.237, // Converting meters/second to mph
     windDirection: data.wind.deg, // degrees
     humidity: data.main.humidity, // percentage
-    visibility: data.visibility / 1000, // Converting meters to kilometers
-    airPressure: data.main.pressure, // hPa
+    visibility: data.visibility / 1609.34, // Converting meters to miles
+    airPressure: data.main.pressure / 100, // Converting hPa to mb
   };
+};
+
+const getWeatherIcon = (data: WeatherObject) => {
+  if (data.weatherIcon === "11d" || data.weatherIcon === "11n")
+    return "Thunderstorm";
+
+  if (data.weatherIcon === "09d" || data.weatherIcon === "09n") {
+    if ([302, 312].includes(data.weatherID)) return "HeavyRain";
+    if ([313, 314, 321, 521, 522, 531].includes(data.weatherID))
+      return "Shower";
+    return "LightRain";
+  }
+
+  if (data.weatherIcon === "10d" || data.weatherIcon === "10n") {
+    if (data.weatherID === 500) return "LightRain";
+    if (data.weatherID === 501) return "Shower";
+    return "HeavyRain";
+  }
+
+  if (data.weatherIcon === "13d" || data.weatherIcon === "13n") {
+    if (data.weatherID === 511) return "Haill";
+    if ([600, 601, 602].includes(data.weatherID)) return "Snow";
+    return "Sleet";
+  }
+
+  if (data.weatherIcon === "50d" || data.weatherIcon === "50n") return "Mist";
+  if (data.weatherIcon === "01d" || data.weatherIcon === "01n") return "Clear";
+
+  if (
+    data.weatherIcon === "02d" ||
+    data.weatherIcon === "02n" ||
+    data.weatherIcon === "03d" ||
+    data.weatherIcon === "03n"
+  )
+    return "LightCloud";
+
+  if (data.weatherIcon === "04d" || data.weatherIcon === "04n")
+    return "HeavyCloud";
+
+  return;
 };
 
 export const loadCurrentLocationWeather = async (position: any) => {
@@ -50,7 +97,11 @@ export const loadCurrentLocationWeather = async (position: any) => {
       forecastRes.json(),
     ]);
 
-    console.log(createWeatherObject(weatherData));
+    console.log(
+      createWeatherObject(weatherData),
+      getWeatherIcon(createWeatherObject(weatherData))
+    );
+    state.weatherData = createWeatherObject(weatherData);
   } catch (err: any) {
     throw err;
   }
